908411010f122d559bdd6f20ccc0369cd4f342e6
vohoangloc123 <you@example.com>
2024-03-30 17:39:09 +0700

    push to fix check connect dynamoDB

--- app/src/main/java/com/example/peachzyapp/dynamoDB/DynamoDBManager.java
+++ app/src/main/java/com/example/peachzyapp/dynamoDB/DynamoDBManager.java
@@ -1,8 +1,50 @@
 package com.example.peachzyapp.dynamoDB;
 
-import com.amazonaws.*;
+import android.content.Context;
+import android.widget.Toast;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.dynamodbv2.document.Table;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 
 public class DynamoDBManager {
+    private Context context;
+    private AmazonDynamoDBClient ddbClient;
 
+    public DynamoDBManager(Context context) {
+        if (context == null) {
+            throw new IllegalArgumentException("Context cannot be null");
+        }
+        this.context = context;
 
+    }
+
+    private void initializeDynamoDB() {
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                context.getApplicationContext(),
+                "thanhnhan16.2.2002@gmail.com", // Replace with your AWS account email or phone number
+                "ap-southeast-1", // Replace with the region your identity pool resides in
+                "AKIAZI2LEH5QMWTQ3VEH", // Replace with your AWS Access Key ID
+                "cI8hmArANH7FOh5AwlC3sqPnV88CZQSw7ndTtS/F", // Replace with your AWS Secret Access Key
+                Regions.valueOf("ap-southeast-1") // Replace with the AWS region you are using
+        );
+        ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+
+        // Check connection status
+//        checkDynamoDBConnection();
+    }
+//
+//    public void checkDynamoDBConnection() {
+//        try {
+//            ListTablesResult tables = ddbClient.listTables();
+//            // If there's no error, connection is successful
+//            Toast.makeText(context, "DynamoDB connection successful.", Toast.LENGTH_SHORT).show();
+//        } catch (AmazonServiceException e) {
+//            // If there's an error, connection failed
+//            Toast.makeText(context, "DynamoDB connection failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+//        }
+//    }
 }
