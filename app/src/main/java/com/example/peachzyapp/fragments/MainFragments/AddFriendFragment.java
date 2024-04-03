51d75dc072be1d577a1dc9ecd8889749cc3457d6
vohoangloc123 <you@example.com>
2024-04-03 22:53:56 +0700

    Fixed the Find Friends interface and Friend Display Adapter after searching and processing the Friend List Display interface, Friend Request ... (Still rudimentary)

--- app/src/main/java/com/example/peachzyapp/fragments/MainFragments/AddFriendFragment.java
+++ app/src/main/java/com/example/peachzyapp/fragments/MainFragments/AddFriendFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,7 +22,7 @@ import java.util.ArrayList;
 
 public class AddFriendFragment extends Fragment {
     EditText etEmail;
-    Button btnFind;
+    ImageButton btnFind;
     private DynamoDBManager dynamoDBManager;
     public static final String TAG1= AddFriendFragment.class.getName();
     private ListView listView; // Assume you're using ListView
